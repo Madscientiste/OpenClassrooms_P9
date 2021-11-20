b3c45bc0226be3e51442a3887d2d050490267e65
Madscientist <s.shaade77@gmail.com>
2021-11-20 14:36:17 +0100

    fixed flux, now users only see posts from users they follow

--- posts/views/flux.py
+++ posts/views/flux.py
@@ -1,18 +1,26 @@
+from django.db.models.query import QuerySet
 from django.http.request import HttpRequest
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
 from accounts.views import LoginRequired
 from posts.models import Review, Ticket
+from accounts.models import UserFollows
 
 
 class FluxPage(LoginRequired, TemplateView):
     template_name = "pages/flux.html"
 
     def get(self, req: HttpRequest):
-        tickets = Ticket.objects.all()
-        reviews = Review.objects.all()
+        following = UserFollows.objects.filter(user=req.user)
+        following = following.values_list("followed_user", flat=True)
 
-        posts = [*tickets, *reviews]
+        # add self to following so we can see our own reviews
+        following = list(following) + [req.user.id]
+
+        reviews = Review.objects.filter(user__in=following)
+        tickets = Ticket.objects.filter(user__in=following)
+
+        posts = [*reviews, *tickets]
 
         return render(req, self.template_name, {"posts": posts})
